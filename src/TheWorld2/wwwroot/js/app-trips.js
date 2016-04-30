ce03af7fc5b3b0b5d482afbc65b819b3a47f0ded
AnTran <an.t.thien@hiof.no>
2016-04-30 20:49:04 +0200

    Added Map

--- src/TheWorld2/wwwroot/js/app-trips.js
+++ src/TheWorld2/wwwroot/js/app-trips.js
@@ -1,5 +1,20 @@
 ﻿(function () {
     "use strict";
     //define a module
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+        .config(function ($routeProvider) { // config the module object that's returned
+            $routeProvider.when("/", {
+                controller: "tripsController",
+                controllerAs: "vm", //alias
+                templateUrl: "/views/tripsView.html"
+            });
+
+            $routeProvider.when("/editor/:tripName", {
+                controller: "tripEditorController",
+                controllerAs: "vm", //alias
+                templateUrl: "/views/tripEditorView.html"
+            });
+
+            $routeProvider.otherwise({ redirectTo: "/" });
+        });
 })();
---
+++ src/TheWorld2/wwwroot/js/tripEditorController.js
@@ -0,0 +1,65 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("app-trips")
+        .controller("tripEditorController", tripEditorController);
+
+    function tripEditorController($routeParams, $http) {
+        var vm = this;
+        vm.tripName = $routeParams.tripName;
+        vm.stops = [];
+        vm.errorMessage = "";
+        vm.isBusy = true;
+        vm.newStop = {};
+
+        var url = "/api/trips/" + vm.tripName + "/stops";
+        $http.get(url)
+        .then(function (response) { //callback for success
+            angular.copy(response.data, vm.stops);
+            _showMap(vm.stops);
+        }, function (error) { //callback for failure
+            vm.errorMessage = "Failed to load the stops: " + error;
+        })
+        .finally(function () {
+            vm.isBusy = false;
+        });
+
+        vm.addStop = function () {
+            vm.isBusy = true;
+
+            vm.errorMessage = "";
+
+            $http.post(url, vm.newStop)
+            .then(function (response) {
+                vm.stops.push(response.data);
+                vm.newStop = {};
+                _showMap(vm.stops);
+            }, function (error) {
+                vm.errorMessage = "Failed to add new stop";
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+        };
+    };
+
+    function _showMap(stops) {
+        if (stops && stops.length > 0) {
+            var mapStops = _.map(stops, function (item) {
+                return {
+                    lat: item.latitude,
+                    long: item.longitude,
+                    info: item.name
+                };
+            });
+            // Show map
+
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
+            });
+        }
+    }
+})();
