a0fc42b2acf67258ca2f219bcfac8d1b84a6318b
AnTran <an.t.thien@hiof.no>
2016-05-05 21:40:12 +0200

    Add gulp & minified JS

---
+++ src/TheWorld2/wwwroot/lib/_app/site.js
@@ -0,0 +1,1 @@
+!function(){var a=$("#sidebar, #wrapper"),e=$("#sideBarToggle i.fa");$("#sideBarToggle").on("click",function(){a.toggleClass("hide-sidebar"),a.hasClass("hide-sidebar")?(e.removeClass("fa-angle-left"),e.addClass("fa-angle-right")):(e.removeClass("fa-angle-right"),e.addClass("fa-angle-left"))})}();
